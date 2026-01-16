a8925ecadb19572e4f39a204f24e705c7a41bed0
Éder Pires <ederpbj@gmail.com>
2026-01-15 23:23:24 -0300

    base do projeto react

---
+++ agenda-studio/src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>Agenda Studio</h1>
+      <p>Micro-SaaS para agendamento de salão de beleza</p>
+    </div>
+  )
+}
